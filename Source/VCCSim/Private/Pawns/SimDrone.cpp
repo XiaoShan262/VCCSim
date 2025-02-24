4efedc40177ba436c56347f3e9690b23f8f4369e
Michael Eret <michael.gonw@gmail.com>
2025-02-24 19:02:08 +0800

    Add: SimDrone

---
+++ Source/VCCSim/Private/Pawns/SimDrone.cpp
@@ -0,0 +1,1 @@
+#include "Pawns/SimDrone.h"
---
+++ Source/VCCSim/Public/Pawns/SimDrone.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DronePawn.h"
+#include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "SimDrone.generated.h"
+
+class ARecorder;
+
+UCLASS()
+class VCCSIM_API ASimDrone : public ADronePawn
+{
+	GENERATED_BODY()
+};
